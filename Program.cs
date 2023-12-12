bfe36b50a19ffc7f219c576f4b82ccdf13398497
hasanbadr <hasan.badr@edu.keuda.fi>
2023-12-12 07:55:33 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static int Jakojäännös(int a, int b)
+    {
+        return a % b;
+    }
+
+    static void Main()
+    {
+        Console.Write("Syötä ensimmäinen numero: ");
+        int numero1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Syötä toinen numero: ");
+        int numero2 = int.Parse(Console.ReadLine());
+
+        int jakojäännös = Jakojäännös(numero1, numero2);
+        Console.WriteLine($"Jakojäännös: {jakojäännös}");
+    }
+}
